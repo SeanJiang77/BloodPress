f62e0eb72ca137679c6ec90682fe46f8aee42c85
SeanJiang77 <SeanJiang2005@gmail.com>
2024-10-05 17:55:58 -0400

    Fix database credentials and enable SSL

--- config.py
+++ config.py
@@ -6,14 +6,9 @@ secret_key = secrets.token_hex(8)
 print(f"Generated SECRET_KEY: {secret_key}")
 
 class Config:
-    # SECRET_KEY，优先从环境变量中获取，如果没有则使用生成的密钥
-    SECRET_KEY = os.environ.get('SECRET_KEY') or secret_key
-
-    # 数据库连接字符串，优先从环境变量获取
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'postgresql://bloodpressuser:SzXPGJf4GTaJzvYH2by4gz0Ku9hiF6iN@dpg-cs0qvqogph6c73adlsrg-a.oregon-postgres.render.com:5432/bloodpressdb?sslmode=require'
-
-    # 禁用SQLAlchemy修改追踪，节省内存
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
