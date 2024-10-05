7b30d176aad9d1e8d109d4e25de3a196e227c237
SeanJiang77 <SeanJiang2005@gmail.com>
2024-10-05 17:52:59 -0400

    Correct SQLALCHEMY_DATABASE_URI and add SSL mode

--- config.py
+++ config.py
@@ -1,10 +1,19 @@
 import os
 import secrets
-secret_key = secrets.token_hex(8)  # 生成一个8字节的简单密钥
-print(secret_key)
+
+# 生成一个8字节的简单密钥（可以用作SECRET_KEY）
+secret_key = secrets.token_hex(8)
+print(f"Generated SECRET_KEY: {secret_key}")
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('postgresql://bloodpressuser:SzXPGJf4GTaJzvYH2by4gz0Ku9hiF6iN@dpg-cs0qvqogph6c73adlsrg-a.oregon-postgres.render.com:5432/bloodpressdb') + '?sslmode=require'
+    # SECRET_KEY，优先从环境变量中获取，如果没有则使用生成的密钥
+    SECRET_KEY = os.environ.get('SECRET_KEY') or secret_key
+
+    # 数据库连接字符串，优先从环境变量获取
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'postgresql://bloodpressuser:SzXPGJf4GTaJzvYH2by4gz0Ku9hiF6iN@dpg-cs0qvqogph6c73adlsrg-a.oregon-postgres.render.com:5432/bloodpressdb?sslmode=require'
+
+    # 禁用SQLAlchemy修改追踪，节省内存
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
