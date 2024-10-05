e97b4c2bf94932279ca92f9459ac2585492097b0
SeanJiang77 <SeanJiang2005@gmail.com>
2024-10-05 17:59:14 -0400

    Update database credentials

--- config.py
+++ config.py
@@ -7,8 +7,7 @@ print(f"Generated SECRET_KEY: {secret_key}")
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'postgresql://bloodpressuser:SzXPGJf4GTaJzvYH2by4gz0Ku9hiF6iN@dpg-cs0qvqogph6c73adlsrg-a.oregon-postgres.render.com:5432/bloodpressdb?sslmode=require'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://bloodpressuser:SzXPGJ14GTaJzvYH2by4gz0Ku9hiF6iN@dpg-cs0qvqogph6c73adlsrg-a.oregon-postgres.render.com:5432/bloodpressdb?sslmode=require'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
