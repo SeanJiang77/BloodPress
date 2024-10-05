2245ab1e4c857842c80543536e50d687f13a7d37
SeanJiang77 <SeanJiang2005@gmail.com>
2024-10-05 17:50:02 -0400

    update config

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
+import secrets
+secret_key = secrets.token_hex(8)  # 生成一个8字节的简单密钥
+print(secret_key)
 
 class Config:
-
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'postgresql://bloodpressuser:SzXPGJf4GTaJzvYH2by4gz0Ku9hiF6iN@dpg-cs0qvqogph6c73adlsrg-a.oregon-postgres.render.com/bloodpressdb'
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get('postgresql://bloodpressuser:SzXPGJf4GTaJzvYH2by4gz0Ku9hiF6iN@dpg-cs0qvqogph6c73adlsrg-a.oregon-postgres.render.com:5432/bloodpressdb') + '?sslmode=require'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
