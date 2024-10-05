defd5405f87f0e3c8ef74f7cfe296ec7bea19419
SeanJiang77 <SeanJiang2005@gmail.com>
2024-10-05 17:39:52 -0400

    config update

--- config.py
+++ config.py
@@ -1,19 +1,10 @@
 import os
-import secrets
-secret_key = secrets.token_hex(8)  # 生成一个8字节的简单密钥
-print(secret_key)
+
 class Config:
-    SECRET_KEY = secret_key
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///local_bloodpressure.db'
 
-class DevelopmentConfig(Config):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///dev_bloodpressure.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
 
-class ProductionConfig(Config):
-    DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://user:password@localhost/prod_bloodpressure_db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'postgresql://bloodpressuser:SzXPGJf4GTaJzvYH2by4gz0Ku9hiF6iN@dpg-cs0qvqogph6c73adlsrg-a.oregon-postgres.render.com/bloodpressdb'
 
-class TestingConfig(Config):
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///test_bloodpressure.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
